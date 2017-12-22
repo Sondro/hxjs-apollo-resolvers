460418edcb7f65d4a590701223a28fa45843b2ac
David Klein <david.klein@psmtec.de>
2017-12-22 09:50:18 +0100

    initial

---
+++ apollo/ApolloResolvers.hx
@@ -0,0 +1,13 @@
+package apollo;
+
+private typedef ResolveFunction = Dynamic -> Dynamic -> Dynamic -> Dynamic -> Void;
+private typedef ErrorFunction = Dynamic -> Void;
+
+interface Resolver {
+    function createResolver( ?res: ResolveFunction, ?err: ErrorFunction ) : Resolver;
+}
+
+@:jsRequire('apollo-resolvers')
+extern class ApolloResolvers {
+    public static function createResolver( ?res: ResolveFunction, ?err: ErrorFunction ) : Resolver;
+}
